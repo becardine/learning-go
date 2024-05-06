5d921bb0e21060073d0238c1dcda99440b224325
becardine <bealvescardine@gmail.com>
2024-05-06 20:49:30 -0300

    feat: Create initial Go program with hello world message and understanding the main.go file

---
+++ fundamentals/first/main.go
@@ -0,0 +1,11 @@
+// todo arquivo precisa ter um pacote, com exceção do main o nome deve ser o mesmo do diretório
+// main é o package principal, o que indica que o arquivo é executável
+// todos os arquivos que estão no mesmo diretório devem ter o mesmo package
+// tudo que está dentro do mesmo package pode ser acessado por outros arquivos do mesmo package
+package main
+
+// função principal que será executada quando o programa for executado
+func main() {
+	// println é uma função que imprime uma linha de texto no console
+	println("Hello, World!")
+}
