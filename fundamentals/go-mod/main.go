34ba11d86b16ea8582e30ea4e1e5ba467fb36fbc
becardine <bealvescardine@gmail.com>
2024-05-09 20:02:12 -0300

    feat: Add go mod tidy command to main.go

--- fundamentals/go-mod/main.go
+++ fundamentals/go-mod/main.go
@@ -2,6 +2,7 @@ package main
 
 // go mod init {module-name}
 // geralmente o nome do módulo é o nome do repositório
+// go mod tidy - remove dependências que não estão sendo utilizadas e adiciona dependências que estão sendo utilizadas
 
 import (
 	"github.com/becardine/learning-go/math"
