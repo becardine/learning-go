71f86990c2362e24cc20c14ecaf4f2ea9e554cc8
becardine <bealvescardine@gmail.com>
2024-05-18 00:02:46 -0300

    chore: Add main package with math operations

---
+++ packaging/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/becardine/learning-go/packaging/math"
+)
+
+// folder cmd is used to store the main package
+
+func main() {
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Add())
+}
---
+++ packaging/go.mod
@@ -0,0 +1,3 @@
+module github.com/becardine/learning-go/packaging
+
+go 1.22.2
