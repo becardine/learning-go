6b4649fbf3b575cb608d7cb100341c699b53977f
becardine <bealvescardine@gmail.com>
2024-05-19 22:16:44 -0300

    chore: Add unit tests for user database find by email functionality

--- apis/internal/infra/database/user_db_test.go
+++ apis/internal/infra/database/user_db_test.go
@@ -28,3 +28,23 @@ func TestCreateUser(t *testing.T) {
 	assert.Equal(t, user.Name, userFound.Name)
 	assert.NotNil(t, userFound.Password)
 }
+
+func TestFindByEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.User{})
+	user, _ := entity.NewUser("John Doe", "password", "john@email.com")
+	userDB := NewUser(db)
+	err = userDB.Create(user)
+	assert.Nil(t, err)
+
+	userFound, err := userDB.FindByEmail(user.Email)
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, userFound.ID)
+	assert.Equal(t, user.Name, userFound.Name)
+	assert.Equal(t, user.Email, userFound.Email)
+	assert.NotNil(t, userFound.Password)
+}
