262e3f2146ee7b5084b3d8d0ec522105a5231d1d
becardine <bealvescardine@gmail.com>
2024-05-20 21:37:52 +0000

    chore: Update main.go to include CreateProduct handler for HTTP requests

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,8 +1,62 @@
 package main
 
-import "github.com/becardine/learning-go/apis/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/becardine/learning-go/apis/configs"
+	"github.com/becardine/learning-go/apis/internal/dto"
+	"github.com/becardine/learning-go/apis/internal/entity"
+	"github.com/becardine/learning-go/apis/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBHost)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProduct(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// normalmente essa parte seria feita em um service
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
