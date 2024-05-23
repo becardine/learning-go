4244f528cdddfc4bade76ffb2ca902c785d6516f
becardine <bealvescardine@gmail.com>
2024-05-23 19:48:15 -0300

    feat: Add event handling interfaces and dispatcher

---
+++ events/pkg/events/event_dispatcher.go
@@ -0,0 +1,27 @@
+package events
+
+import "errors"
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) AddListener(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
