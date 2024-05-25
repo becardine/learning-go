65f3552d5ce92dd1edc5d7b071c4b729c1df0efc
becardine <bealvescardine@gmail.com>
2024-05-24 23:10:56 -0300

    feat: Add RabbitMQ consumer for handling messages

---
+++ events/cmd/consumer/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/becardine/learning-go/events/utils/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
+	for m := range out {
+		fmt.Println(string(m.Body))
+		m.Ack(false) // acknowledge the message to rabbitmq
+	}
+}
