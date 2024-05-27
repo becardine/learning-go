9f8e226c084653f1ba93dfef22c798b6d3dc2a52
becardine <bealvescardine@gmail.com>
2024-05-27 19:10:05 -0300

    feat: Add CategoryService to handle gRPC requests for category creation

---
+++ grpc/internal/service/category.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+
+	"github.com/becardine/learning-go/grpc/internal/database"
+	"github.com/becardine/learning-go/grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (s *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, err := s.CategoryDB.Create(in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := &pb.CategoryResponse{
+		Category: &pb.Category{
+			Id:          category.ID,
+			Name:        category.Name,
+			Description: category.Description,
+		},
+	}
+	return categoryResponse, nil
+}
