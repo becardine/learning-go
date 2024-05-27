31edbd52f5f5093561365fc8a623d98c641139d9
becardine <bealvescardine@gmail.com>
2024-05-27 19:15:41 -0300

    feat: Add gRPC server to handle category service requests

---
+++ grpc/cmd/grpcServer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/becardine/learning-go/grpc/internal/database"
+	"github.com/becardine/learning-go/grpc/internal/pb"
+	"github.com/becardine/learning-go/grpc/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	// Start gRPC server
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
+}
