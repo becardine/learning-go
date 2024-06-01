5f9921c56a5323753c7d079d9fce185a126fba01
becardine <bealvescardine@gmail.com>
2024-06-01 13:37:17 -0300

    feat: Add SQLC migrations for database setup

---
+++ uow/internal/repository/category.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/becardine/learning-go/uow/internal/db"
+	"github.com/becardine/learning-go/uow/internal/entity"
+)
+
+type CategoryRepositoryInterface interface {
+	Insert(ctx context.Context, category entity.Category) error
+}
+
+type CategoryRepository struct {
+	DB      *sql.DB
+	Queries *db.Queries
+}
+
+func NewCategoryRepository(dtb *sql.DB) *CategoryRepository {
+	return &CategoryRepository{
+		DB:      dtb,
+		Queries: db.New(dtb),
+	}
+}
+
+func (r *CategoryRepository) Insert(ctx context.Context, category entity.Category) error {
+	return r.Queries.CreateCategory(ctx, db.CreateCategoryParams{
+		Name: category.Name,
+	})
+}
