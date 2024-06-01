d71908d9bd1883657124f09ae7bb25b4af5097c6
becardine <bealvescardine@gmail.com>
2024-06-01 14:27:34 -0300

    feat: Refactor Uow struct and methods for better code organization

--- uow/pkg/uow/uow.go
+++ uow/pkg/uow/uow.go
@@ -3,6 +3,8 @@ package uow
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
 )
 
 // fabrica que retorna um repositório
@@ -11,7 +13,7 @@ type RepositoryFactory func(tx *sql.Tx) interface{}
 type UowInterface interface {
 	Register(name string, fc RepositoryFactory)
 	GetRepository(ctx context.Context, name string) (interface{}, error)
-	Do(ctx context.Context, fn func(uow UowInterface) error) error
+	Do(ctx context.Context, fn func(uow *Uow) error) error
 	CommitOrRollback() error
 	Rollback() error
 	UnRegister(name string)
@@ -36,4 +38,68 @@ func (u *Uow) Register(name string, fc RepositoryFactory) {
 
 func (u *Uow) UnRegister(name string) {
 	delete(u.Repositories, name)
+}
+
+func (u *Uow) GetRepository(ctx context.Context, name string) (interface{}, error) {
+	if u.Tx == nil {
+		tx, err := u.Db.BeginTx(ctx, nil)
+		if err != nil {
+			return nil, err
+		}
+		u.Tx = tx
+	}
+	repo := u.Repositories[name](u.Tx)
+	return repo, nil
+}
+
+func (u *Uow) Do(ctx context.Context, fn func(uow *Uow) error) error {
+	// verifica se a transação já foi iniciada
+	if u.Tx != nil {
+		return fmt.Errorf("transaction already started")
+	}
+	// inicia a transação
+	tx, err := u.Db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	u.Tx = tx
+
+	// executa a função passando o uow
+	err = fn(u)
+	if err != nil {
+		if rbErr := u.Tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("transaction error: %v, rollback error: %v", err.Error(), rbErr.Error())
+		}
+		return err
+	}
+
+	return u.CommitOrRollback()
+}
+
+func (u *Uow) Rollback() error {
+	if u.Tx == nil {
+		return errors.New("transaction not started")
+	}
+	err := u.Tx.Rollback()
+	if err != nil {
+		return err
+	}
+	u.Tx = nil
+	return nil
+}
+
+func (u *Uow) CommitOrRollback() error {
+	if u.Tx == nil {
+		return errors.New("transaction not started")
+	}
+	err := u.Tx.Commit()
+	if err != nil {
+		if rbErr := u.Tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("commit error: %v, rollback error: %v", err.Error(), rbErr.Error())
+		}
+		return err
+	}
+	u.Tx = nil
+	return nil
 }
